f3337ac5de3591ff870b03535d45903961a290b7
renoeno <reno.less@gmail.com>
2022-02-18 15:54:24 -0300

    Change about page

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -28,30 +28,33 @@ const About = () => {
             <div className={styles.picture}>
               <Image
                 src="/picture.jpg"
-                width="180"
-                height="210"
+                width="2000"
+                height="1333"
                 alt="Foto pessoal"
                 priority={true}
               />
             </div>
             <Contacts color="black" />
           </div>
-          <p className="content-text">
-            Meu nome é Reno Almeida e sou desenvolvedor e pesquisador. Sou
-            graduado em Sistemas e Mídias Digitais pela UFC, onde desenvolvi
-            diversos projetos e me foquei em trabalhos na interseção entre
-            tecnologia, arte e design. Sou mestre em Artes também pela UFC, e
-            pesquisei a relação entre dados e ferramentas de controle,
-            desenvolvendo um conjunto de projetos (web, eletrônico, foto,
-            performance...) que problematizam o assunto. Curso doutorado em
-            Comunicação pela UFPE, ao mesmo tempo que desenvolvo projetos
-            (especialmente web) que põem em prática minha formação
-            interdisciplinar. Acima você encontra diferentes links para contato.
-            <p>
-              Este site foi feito utilizando o framework NextJS, Sass e Framer
-              Motion.
+          <div className={styles.contentText}>
+            <p className="content-text">
+              Meu nome é Reno Almeida e sou desenvolvedor e pesquisador. Sou
+              graduado em Sistemas e Mídias Digitais pela UFC, onde desenvolvi
+              diversos projetos e me foquei em trabalhos na interseção entre
+              tecnologia, arte e design. Sou mestre em Artes também pela UFC, e
+              pesquisei a relação entre dados e ferramentas de controle,
+              desenvolvendo um conjunto de projetos (web, eletrônico, foto,
+              performance...) que problematizam o assunto. Curso doutorado em
+              Comunicação pela UFPE, ao mesmo tempo que desenvolvo projetos
+              (especialmente web) que põem em prática minha formação
+              interdisciplinar. Aqui você encontra diferentes links para
+              contato.
+              <p>
+                Este site foi feito utilizando o framework NextJS, Sass e Framer
+                Motion.
+              </p>
             </p>
-          </p>
+          </div>
         </motion.div>
       </motion.div>
     </>
